efeb3d1f3fe7928469466c94a906bbffb0e639e5
Christian <chris_veenman@hotmail.com>
2015-12-01 15:57:47 +0100

    3D Camera added, enhanced #5

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -8,6 +8,8 @@ public class SmoothFollow : MonoBehaviour
 {
     // The target we are following
     public Transform target;
+    private Vector3 position;
+
     // The distance in the x-z plane to the target
     public float distance = 10.0f;
     // the height we want the camera to be above the target
@@ -15,6 +17,8 @@ public class SmoothFollow : MonoBehaviour
     // How much we 
     public float heightDamping = 2.0f;
     public float rotationDamping = 3.0f;
+
+ 
 
     // Place the script in the Camera-Control group in the component menu
     [AddComponentMenu("Camera-Control/Smooth Follow")]
@@ -42,13 +46,27 @@ public class SmoothFollow : MonoBehaviour
 
         // Set the position of the camera on the x-z plane to:
         // distance meters behind the target
-        transform.position = target.position;
-        transform.position -= currentRotation * Vector3.forward * distance;
+        //transform.position = target.position;
+        //transform.position -= currentRotation * Vector3.forward * distance;
+        //transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 
-        // Set the height of the camera
-        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+        Vector3 direction = position - target.position;
+        transform.position -= direction;
 
-        // Always look at the target
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(target.position, target.up, Input.GetAxis("Mouse X") * 10.0f);
+            transform.RotateAround(target.position, target.right, -Input.GetAxis("Mouse Y") * 5.0f);
+        }
+
+        // Always look at the target from a specific distance.
         transform.LookAt(target);
+        
+
+        // Update the forward and right vectors of the entity to make it move with the camera.
+        target.forward = transform.forward;
+        target.right = transform.right;
+
+        position = target.position;
     }
 }
