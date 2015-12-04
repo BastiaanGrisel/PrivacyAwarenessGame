d2b40b934cb97ff68eecbaa31c3db04df3a81376
Ruben Vroegindeweij <ruben_vroegindeweij@hotmail.com>
2015-12-04 15:53:48 +0100

    Adds the controllers from the NewNetworking

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+    [SerializeField]
+    private Camera cam;
+    
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Gets a movement vector
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    // Gets a rotation vector
+    public void Rotate(Vector3 rotation)
+    {
+        this.rotation = rotation;
+    }
+
+    // Gets a rotation vector for the camera
+    public void RotateCamera(Vector3 cameraRotation)
+    {
+        this.cameraRotation = cameraRotation;
+    }
+
+    // Run every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    // Perform movement based on velocity veriable
+    void PerformMovement()
+    {
+        if (!velocity.Equals(Vector3.zero))
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+	private Behaviour[] componetsToDisable;
+    private Camera sceneCamera;
+
+    void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour comp in componetsToDisable){
+                comp.enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+	}
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
