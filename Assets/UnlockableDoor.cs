2fc7209671af413d0373a057603de49b0718f4c7
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-12-04 16:10:13 +0100

    Text-based door

---
+++ Assets/UnlockableDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockableDoor : MonoBehaviour {
+
+	public int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(counter == 3)
+			gameObject.SetActive (false);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		
+	}
+
+}
