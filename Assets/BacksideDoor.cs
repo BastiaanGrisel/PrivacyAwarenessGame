d856fac49d4dd186fb46f46606706e059037c252
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-12-15 17:21:02 +0100

    Doors are always open form the backside #14

---
+++ Assets/BacksideDoor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BacksideDoor : MonoBehaviour {
+
+	public GameObject Door;
+
+	void OnTriggerEnter(Collider other) {
+		Door.GetComponent<BoxCollider> ().enabled = false;
+	}
+
+	void OnTriggerExit(Collider other) {
+		Door.GetComponent<BoxCollider> ().enabled = true;
+	}
+}
